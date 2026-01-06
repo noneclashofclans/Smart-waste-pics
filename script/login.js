aab6e559d4beced0f7bf6765d5e158bc3e1d6b74
noneclashofclans <rishitmohanty3@gmail.com>
2026-01-06 23:28:47 +0530

    Made changes

--- script/login.js
+++ script/login.js
@@ -10,7 +10,7 @@ submit_btn.addEventListener('click', async () => {
     }
 
     try {
-        const response = await fetch('http://localhost:7000/api/login', {
+        const response = await fetch('https://smart-waste-pics-authentication-user.onrender.com/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
--- script/register.js
+++ script/register.js
@@ -10,7 +10,7 @@ submit_btn.addEventListener('click', async () => {
     }
 
     try{
-        const response = await fetch('http://localhost:7000/api/register', {
+        const response = await fetch('https://smart-waste-pics-authentication-user.onrender.com/api/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
