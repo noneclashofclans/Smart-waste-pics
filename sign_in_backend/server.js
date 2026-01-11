723fcaac6691d83258dcac49de2552e71ed6900b
noneclashofclans <rishitmohanty3@gmail.com>
2026-01-11 18:19:43 +0530

    Chats added!!

--- sign_in_backend/server.js
+++ sign_in_backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://smart-waste-pics-pfy6.vercel.app/", 
+        origin: "https://smart-waste-pics-pfy6.vercel.app", 
         methods: ["GET", "POST"]
     }
 });
