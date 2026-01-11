1dca6c83ec63e25c32dbe9fbe8845814e2ae2611
noneclashofclans <rishitmohanty3@gmail.com>
2026-01-12 01:05:35 +0530

    Added webhook URL

--- script/script.js
+++ script/script.js
@@ -29,7 +29,8 @@ try {
     console.warn("Socket.IO initialization failed:", e);
 }
 
-const N8N_WEBHOOK_URL = "https://n8n-smart-waste-pics.onrender.com/webhook/61e29fbc-00ef-4ab5-9d0a-ac1c416eb8c7";
+
+const N8N_WEBHOOK_URL = "https://n8n-2-rypn.onrender.com/webhook/61e29fbc-00ef-4ab5-9d0a-ac1c416eb8c7";
 const ML_PREDICTION_URL = "https://smart-waste-pics-1.onrender.com/predict";
 
 let imageUploaded = false;
